c30a8454f6e68aea827c79445e5c87e117fdaada
Odinn <goryn.alexander@gmail.com>
2017-07-03 01:43:30 +0300

    Changed lib for CircledImageView, added onClick to items, passed data to UserActivity (full profile)

--- app/src/main/java/com/goryn/randomapplication/adapters/UserAdapter.java
+++ app/src/main/java/com/goryn/randomapplication/adapters/UserAdapter.java
@@ -1,18 +1,24 @@
 package com.goryn.randomapplication.adapters;
 
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.alexzh.circleimageview.CircleImageView;
+
+import com.google.gson.Gson;
 import com.goryn.randomapplication.models.User;
 import com.goryn.randomapplication.R;
 import com.goryn.randomapplication.models.UserInfo;
+import com.goryn.randomapplication.ui.UserActivity;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by Odinn on 29.06.2017.
@@ -44,6 +50,16 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
                     .load(imageURL)
                     .resize(50,50)
                     .into(holder.circleImageView);
+
+            holder.itemLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String userObject = new Gson().toJson(user);
+                    Intent intent = new Intent(view.getContext(), UserActivity.class);
+                    intent.putExtra("user", userObject);
+                    view.getContext().startActivity(intent);
+                }
+            });
         } catch (NullPointerException ignored){
 
         }
@@ -57,10 +73,12 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
     public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView tvTestText;
         CircleImageView circleImageView;
+        RelativeLayout itemLayout;
         public ViewHolder(View itemView) {
             super(itemView);
             tvTestText = (TextView) itemView.findViewById(R.id.tvName);
             circleImageView = (CircleImageView) itemView.findViewById(R.id.ivThumbnail);
+            itemLayout = (RelativeLayout) itemView.findViewById(R.id.rlItemLayout);
         }
     }
 }
--- app/src/main/java/com/goryn/randomapplication/ui/UserActivity.java
+++ app/src/main/java/com/goryn/randomapplication/ui/UserActivity.java
@@ -1,9 +1,13 @@
 package com.goryn.randomapplication.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.goryn.randomapplication.R;
+import com.goryn.randomapplication.models.UserInfo;
 
 public class UserActivity extends AppCompatActivity {
 
@@ -11,5 +15,10 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        Intent intent = getIntent();
+        String userJson = intent.getStringExtra("user");
+        UserInfo user = new Gson().fromJson(userJson, UserInfo.class);
+        Toast.makeText(this, user.getResults().get(0).getName().getFirst(), Toast.LENGTH_SHORT).show();
     }
 }
