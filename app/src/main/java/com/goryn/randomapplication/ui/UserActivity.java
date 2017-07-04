8b371fb0cc5f1d78bf5b2a5d62bafcfb4b2721a3
Odinn <goryn.alexander@gmail.com>
2017-07-04 14:41:07 +0300

    Filled all important information about user. Added possibility to copy text in textviews

--- app/src/main/java/com/goryn/randomapplication/ui/UserActivity.java
+++ app/src/main/java/com/goryn/randomapplication/ui/UserActivity.java
@@ -23,12 +23,6 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class UserActivity extends AppCompatActivity {
     private UserInfo user;
 
-    private CircleImageView ivAvatar;
-    private TextView tvBDay;
-    private TextView tvGender;
-    private TextView tvState;
-    private TextView tvCity;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,11 +39,23 @@ public class UserActivity extends AppCompatActivity {
     }
 
     private void initUI() {
-        ivAvatar = (CircleImageView) findViewById(R.id.ivAvatar);
-        tvBDay = (TextView) findViewById(R.id.tvBday);
-        tvGender = (TextView) findViewById(R.id.tvGender);
-        tvState = (TextView) findViewById(R.id.tvState);
-        tvCity = (TextView) findViewById(R.id.tvCity);
+        CircleImageView ivAvatar = (CircleImageView) findViewById(R.id.ivAvatar);
+        TextView tvBDay = (TextView) findViewById(R.id.tvBday);
+        TextView tvGender = (TextView) findViewById(R.id.tvGender);
+        TextView tvState = (TextView) findViewById(R.id.tvState);
+        TextView tvCity = (TextView) findViewById(R.id.tvCity);
+        TextView tvStreet = (TextView) findViewById(R.id.tvStreet);
+        TextView tvPostcode = (TextView) findViewById(R.id.tvPostcode);
+        TextView tvEmail = (TextView) findViewById(R.id.tvEmail);
+        TextView tvPhone = (TextView) findViewById(R.id.tvPhone);
+        TextView tvUsername = (TextView) findViewById(R.id.tvUsername);
+        TextView tvPassword = (TextView) findViewById(R.id.tvPassword);
+        TextView tvSalt = (TextView) findViewById(R.id.tvSalt);
+        TextView tvMD5 = (TextView) findViewById(R.id.tvMD5);
+        TextView tvSha1 = (TextView) findViewById(R.id.tvSha1);
+        TextView tvSha256 = (TextView) findViewById(R.id.tvSha256);
+
+
         Result result = user.getResults().get(0);
 
 //        String dateString = "03/26/2012 11:49:00 AM";
@@ -63,16 +69,29 @@ public class UserActivity extends AppCompatActivity {
 //        }
 //        System.out.println(convertedDate);
 
-        getSupportActionBar().setTitle(result.getName().getTitle() + " " + result.getName().getFirst()+ " " + result.getName().getLast());
+        try {
+            getSupportActionBar().setTitle(result.getName().getTitle() + " " + result.getName().getFirst()+ " " + result.getName().getLast());
 
-        Picasso.with(this)
-                .load(result.getPicture().getLarge())
-                .resize(300, 300)
-                .into(ivAvatar);
-        tvBDay.setText(result.getDob());
-        tvGender.setText(result.getGender());
-        tvState.setText(result.getLocation().getState());
-        tvCity.setText(result.getLocation().getCity());
+            Picasso.with(this)
+                    .load(result.getPicture().getLarge())
+                    .resize(300, 300)
+                    .into(ivAvatar);
+            tvBDay.setText(result.getDob());
+            tvGender.setText(result.getGender());
+            tvState.setText(result.getLocation().getState());
+            tvCity.setText(result.getLocation().getCity());
+            tvStreet.setText(result.getLocation().getStreet());
+            //tvPostcode.setText(result.getLocation().getPostcode());
+            tvEmail.setText(result.getEmail());
+            tvPhone.setText(result.getPhone());
+            tvUsername.setText(result.getLogin().getUsername());
+            tvPassword.setText(result.getLogin().getPassword());
+            tvMD5.setText(result.getLogin().getMd5());
+            tvSha1.setText(result.getLogin().getSha1());
+            tvSha256.setText(result.getLogin().getSha256());
+            tvSalt.setText(result.getLogin().getSalt());
+        } catch (NullPointerException ignored){}
+
 
     }
 }
